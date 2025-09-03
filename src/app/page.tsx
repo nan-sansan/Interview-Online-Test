aa501e5b7cfb938cfefd4675e3e3f3c512eb7c54
DESKTOP-QEOMG0Q\NJ <a85124236@gmail.com>
2025-09-03 13:15:26 +0800

    首頁流程修改

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,28 @@
 "use client";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+
 import { useAuthStore } from "@/stores/userStore";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function Home() {
-  const router = useRouter();
+export default function WelcomePage() {
+  const workRoute = [
+    { path: "typeAccount", display: "帳號輸入" },
+    { path: "/register", display: "註冊" },
+    { path: "/login", display: "登入" },
+    { path: "/userList", display: "用戶列表" },
+  ];
   const { user } = useAuthStore();
-
-  useEffect(() => {
-    if (user) {
-      console.log(user);
-      router.push("/Welcome");
-    }
-  }, [router, user]);
-
-  const [value, setValue] = useState("");
-  const [submitted, setSubmitted] = useState("");
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const regex = /^[A-Za-z0-9]*$/; // 正則表達，只允許英文&數字
-    if (regex.test(e.target.value)) {
-      setValue(e.target.value);
-    }
-  };
-
-  const handleSubmit = () => {
-    if (!value.trim()) return;
-    setSubmitted(value.trim());
-    useAuthStore.getState().login({ name: value.trim() });
-  };
-
   return (
-    <div className="flex flex-col gap-2 mx-auto w-[50vw] ">
-      {submitted ? (
-        <div className="text-xl font-bold text-center">Hi, {submitted}!</div>
-      ) : (
-        <>
-          <Label htmlFor="acc">請輸入名稱</Label>
-          <Input
-            id="acc"
-            type="text"
-            placeholder="帳號名稱，僅限英文數字"
-            value={value}
-            onChange={handleInputChange}
-          ></Input>
-          <div className="flex justify-end">
-            <Button className="w-20" onClick={handleSubmit}>
-              送出
-            </Button>
-          </div>
-        </>
-      )}
+    <div className="w-[70vw] p-[20px] mx-auto shadow-md flex flex-col items-center justify-center gap-5 rounded-md bg-indigo-50/50">
+      {user && user.name ? <h1 className="text-xl">HI！{user?.name}</h1> : ""}
+      <div className="flex gap-6">
+        {workRoute.map((route, index) => (
+          <Link href={route.path} key={index}>
+            <div className="w-30 h-20 shadow-md flex items-center justify-center text-xl bg-white rounded-md hover:bg-gray-50/50">
+              {route.display}
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
